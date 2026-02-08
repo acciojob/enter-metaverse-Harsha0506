b9d38729417aae9ca26b3963353243aa8a3d3275
Harsha0506 <110712960+Harsha0506@users.noreply.github.com>
2026-02-08 16:18:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let button = document.getElementById("enterBtn")
+let number = document.getElementById("status")
+
+button.addEventListener("click",function(){
+    number.innerHTML = "<h1> Entered metaverse </h1>"
+})
+
+
